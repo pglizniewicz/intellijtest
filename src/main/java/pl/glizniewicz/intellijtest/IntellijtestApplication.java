fd7446de7724f59ff9aeb7f4d61addaf32c2a126
piotrek <p.glizniewicz@gmail.com>
2023-12-06 12:04:28 +0100

    first commit

---
+++ src/main/java/pl/glizniewicz/intellijtest/IntellijtestApplication.java
@@ -0,0 +1,13 @@
+package pl.glizniewicz.intellijtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/glizniewicz/intellijtest/IntellijtestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.glizniewicz.intellijtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
